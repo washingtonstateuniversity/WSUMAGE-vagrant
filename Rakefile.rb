782a274c016d69a0f1ad35283afbe2561cbca560
Jeremy Bass <jeremybass26@gmail.com>
2013-10-10 08:57:51 -0700

    added new parts
    now when you ask for a password it hides it as when using the LDAP we
    use real ones.  Also MD5 hashes the pass.  Loads all the vagrant plugins
    and ruby gems as needed.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,66 +1,6 @@
-#load 'rake/helper.rb'
-
-class MAGEINSTALLER
-
-  def add_setting(opts=nil)
-    target  = "scripts/install_settings.sh"
-    #content = <<-RUBY
-    #  puts "I'm the target!"
-    #RUBY
-    
-    File.open(target, "a+") do |f|
-      f.write(opts)
-    end
-  end
-
-    def download(from,to)
-        mi =MAGEINSTALLER.new
-    
-        require 'net/http'
-        require 'uri'
-
-        counter = 0
-
-        url_base = from.split('/')[2]
-        url_path = '/'+from.split('/')[3..-1].join('/')
-             
-        puts "starting download for #{url_path}"
-        Net::HTTP.start(url_base) do |http|
-          begin
-            file = open(to, 'wb')
-            file.binmode
-
-            http.request_get(url_path) do |response|
-                size = 0
-                total = response.header["Content-Length"].to_f
-              response.read_body do |chunk|
-                    file << chunk
-                    counter += 1
-                    size += chunk.size
-                    new_progress = (size.to_f / total.to_f * 100.0).round(2)
-                    progress = "#" * (new_progress / 10)
-                    print "#{progress} --#{new_progress}%--\r"
-                    #$stdout.flush
-                  #sleep 0.005 
-              end
-            end
-          ensure
-            file.close
-          end
-        end
-  
-    end
-  
-    
-    
-end
-
-
-
+load 'rake/helper.rb'
 =begin
-
     Maybe what is needed is a first run type thing.  Test for everything
-    
     task list
         :test #this is for testing that all the plugins are there, if not then install them?
         :create_install_settings #create the settings for the installer
@@ -71,29 +11,66 @@ end
         :clean_db
         :clean_mage_intsall
         :create_install_settings
-
-
 =end
 
+desc "prepare  the system"
 task :init do
-    
     mi =MAGEINSTALLER.new
-    mi.download('http://hc-vagrant-files.s3.amazonaws.com/precise32.box','_BOXES/precise32.box')
-    output = `vagrant init`
-    puts output
-
+    
+    file='_BOXES/precise32.box'
+    if !File.exist?(file)
+        mi.download('http://hc-vagrant-files.s3.amazonaws.com/precise32.box',file)
+    else
+        puts "base box esited"
+    end
+    output = `vagrant plugin list`
+    if !output.include? "vagrant-hostsupdater"
+        puts "installing vagrant-hostsupdater plugin"
+        output = `vagrant plugin install vagrant-hostsupdater`
+        puts output
+    else
+        puts "skipping vagrant-hostsupdater plugin, installed already"
+    end
+    
+    output = `gem list`
+    if !output.include? "highline"
+        puts "installing highline gem"
+        output = `gem install highline`
+        puts output
+    else
+        puts "skipping highline gem, installed already"
+    end
+    output = `gem list`
+    if !output.include? "launchy"
+        puts "installing launchy gem"
+        output = `gem install launchy`
+        puts output
+    else
+        puts "skipping launchy gem, installed already"
+    end
+    
+    puts "cearting default folders"
+    mi.create_file("/www/")
+        
+    puts "> We have settup everything, only last"
+    puts "> thing to do when ready is `rake start`"
+    puts "Would you like to start? [y/n]"
+    input = STDIN.gets.strip
+    if input == 'y'
+        Rake::Task["start"].reenable
+        Rake::Task["start"].invoke
+    end
 end
 
+desc "test  the system"
 task :test do
     puts "the lotion on the skin"
-
 end
-
 
 
 desc "Set up the VM"
 task :start do
-
+    require 'fileutils'
     mi =MAGEINSTALLER.new
     
     puts "Should past MAGE installs be cleared? [y/n]"
@@ -116,8 +93,9 @@ task :start do
     input = STDIN.gets.strip
     if input == 'y'
         target  = "scripts/install_settings.sh"
-        FileUtils.rm_rf(target)
-        puts "database is clean" 
+        file = File.join(Dir.pwd, target)
+        FileUtils.rm_rf(file)
+        puts "removed file #{file}" 
           
         Rake::Task["create_install_settings"].reenable
         Rake::Task["create_install_settings"].invoke
@@ -127,13 +105,24 @@ task :start do
       puts "using the past installer settings"
     end
 
-
+    puts "Finished lets go [any key to continue]"
+    input = STDIN.gets.strip
+    
     exec 'vagrant up'   ## for now just start here but look to 
                         ## http://tech.natemurray.com/2007/03/ruby-shell-commands.html or
                         ## or try to get to vagrant CLI
 end
 
-desc "Destory the vagrant with call back functions run"
+#note just for testing
+task :open do
+    require 'launchy'
+    Launchy.open("http://local.mage.dev/admin")
+end
+
+
+
+
+desc "Destroy the vagrant with call back functions run"
 task :end do
     exec 'vagrant destroy -f'
     puts "Should the databases be cleared? [y/n]"
@@ -146,29 +135,38 @@ task :end do
 end
 
 
-
+desc "clean the database"
 task :clean_db do
   puts "cleaning the database"
   FileUtils.rm_rf(Dir.glob('database/data/*'))
   puts "database is clean"
 end
 
+#maybe abstract this of other apps
+desc "clean the database"
 task :clean_mage_intsall do
   puts "cleaning the MAGE install"
   FileUtils.rm_rf(Dir.glob('www/mage/*'))
   puts "The Magento install has been removed"
 end
 
+
+desc "push to production"#note this would be from a setting file
 task :deploy do
    puts "push to the server"
 end
 
+desc "pull from production"#note this would be from a setting file
 task :pull do
    puts "grabbing files and things"
 end
 
-
+desc "Create a setting file"#maybe abstract this 
 task :create_install_settings do
+    require 'highline/import'
+    require 'digest/md5'
+
+    
     mi =MAGEINSTALLER.new
 
     #MAGEversion="1.8.0.0"
@@ -183,15 +181,15 @@ task :create_install_settings do
     #adminlname="Lovin"
     #adminemail="test.user@wsu.edu"
     mi.add_setting("#!/bin/bash\n")
+    mi.add_setting("MAGEversion=\"1.8.0.0\"\n")
+    mi.add_setting("url=\"local.mage.dev\"\n")
 
-    puts "What version of mage? [default 1.8.0.0]"
-    input = STDIN.gets.strip
-    if input == ''
-        mi.add_setting("MAGEversion=\"1.8.0.0\"\n")
-    else
-        mi.add_setting("MAGEversion=\"input\"\n")
-    end
-
+    mi.add_setting("adminuser=\"admin\"\n")
+    mi.add_setting("adminpass=\"admin2013\"\n")
+    mi.add_setting("adminfname=\"MC\"\n")
+    mi.add_setting("adminlname=\"Lovin\"\n")
+    mi.add_setting("adminemail=\"test.user@wsu.edu\"\n")
+    
     puts "database host of mage? [default localhost]"
     input = STDIN.gets.strip
     if input == ''
@@ -200,7 +198,7 @@ task :create_install_settings do
         #now we'll localhost
         mi.add_setting("dbhost=\"localhost\"\n")
     else
-        mi.add_setting("dbhost=\"input\"\n")
+        mi.add_setting("dbhost=\"#{input}\"\n")
     end
 
     puts "database name of mage? [default mage]"
@@ -208,16 +206,15 @@ task :create_install_settings do
     if input == ''
         mi.add_setting("dbname=\"mage\"\n")
     else
-        mi.add_setting("dbname=\"input\"\n")
+        mi.add_setting("dbname=\"#{input}\"\n")
     end
-
 
     puts "database user of mage? [default devsqluser]"
     input = STDIN.gets.strip
     if input == ''
         mi.add_setting("dbuser=\"devsqluser\"\n")
     else
-        mi.add_setting("dbuser=\"input\"\n")
+        mi.add_setting("dbuser=\"#{input}\"\n")
     end
 
     puts "database user's password of mage? [default devsqluser]"
@@ -225,19 +222,50 @@ task :create_install_settings do
     if input == ''
         mi.add_setting("dbpass=\"devsqluser\"\n")
     else
-        mi.add_setting("dbpass=\"input\"\n")
+        mi.add_setting("dbpass=\"#{input}\"\n")
     end
 
-
-
-
-    mi.add_setting("url=\"local.mage.dev\"\n")
-    mi.add_setting("adminuser=\"admin\"\n")
-    mi.add_setting("adminpass=\"admin2013\"\n")
-    mi.add_setting("adminfname=\"MC\"\n")
-    mi.add_setting("adminlname=\"Lovin\"\n")
+    puts "SAMPLE DATA *** would you like to install this?[y/n]"
+    input = STDIN.gets.strip
+    if input == 'y'
+        mi.add_setting("dbpass=\"true\"\n")
+    else
+        mi.add_setting("dbpass=\"false\"\n")
+    end
     
-    mi.add_setting("adminemail=\"test.user@wsu.edu\"\n")
+    puts "Add your own personal user?[y/n]  *** the default user is still installed ***"
+    input = STDIN.gets.strip
+    if input == 'y'
+        puts "Username: *** This must be your NID if using LDAP ***"
+        uinput = STDIN.gets.strip
+        if uinput != ''
+            mi.add_setting("adminuser=\"#{uinput}\"\n")
+        end
+        
+        #so here is a good example of where we want to pull out so we can check and loop
+        pass = ask("Enter your password: *** must be alphanumeric *** When using LDAP it's your AD password ***") { |q| q.echo = "x" }
+        pass2 = ask("RE-Enter your password:") { |q| q.echo = "x" }
+        if pass != '' && pass == pass
+            pass=Digest::MD5.hexdigest(pass) #don't want usernames hanging around
+            mi.add_setting("adminpass=\"#{pass}\"\n")
+        end  
+
+        puts "First name:"
+        uinput = STDIN.gets.strip
+        if uinput != ''
+            mi.add_setting("adminfname=\"#{uinput}\"\n")
+        end      
+        puts "Last name:"
+        uinput = STDIN.gets.strip
+        if uinput != ''
+            mi.add_setting("adminlname=\"#{uinput}\"\n")
+        end
+    end
+    
+    
+
+    
+    
 
     puts "the installer setting have been created"
 end
--- rake/helper.rb
+++ rake/helper.rb
@@ -1,62 +1,60 @@
-#NOTA: this is actually rake/helper.rb but gists can't named files as directories
+class MAGEINSTALLER
 
-require 'vagrant'
-
-class VM
-  def initialize(opts=nil)
-    opts ||= {ui_class: Vagrant::UI::Colored}
-    @env = Vagrant::Environment.new(opts)
-    @vm = @env.primary_vm
-  end
-
-  def project_path
-    @vm.config.vm.shared_folders['v-project'][:guestpath]
-  end
-
-  def check_if_alive
-    raise "Must run `vagrant up`" if !@vm.created?
-    raise "Must be running!" if @vm.state != :running
-  end
-
-  def cli(command=nil)
-    @env.cli(command) if command
-  end
-
-  def sudo(command=nil)
-    check_if_alive
-    @vm.channel.sudo(command) if command
-  end
-
-  def execute(command=nil, cwd=nil)
-    check_if_alive
-    cwd ||= project_path
-    command = "cd #{cwd}; #{command}"
-
-    # Some code borrowed from Vagrant::Command::SSH.ssh_execute 
-    exit_status = 0
-    
-    exit_status = @vm.channel.execute(command, :error_check => false) do |type, data|
-      channel = type == :stdout ? :out : :error
-      case type
-      when :stdout
-        color = :green
-      when :stderr
-        color = :red
-      else
-        color = :clear
-      end
-
-      # Print the SSH output as it comes in, but don't prefix it and don't
-      # force a new line so that the output is properly preserved
-      @vm.ui.info(data.to_s,
-                  :prefix   => false,
-                  :new_line => false,
-                  :channel  => channel,
-                  :color    => color)
+    def add_setting(opts=nil)
+        target  = "scripts/install_settings.sh"
+        #content = <<-RUBY
+        #  puts "I'm the target!"
+        #RUBY
+        
+        File.open(target, "a+") do |f|
+          f.write(opts)
+        end
     end
 
-    # Exit with the exit status we got from executing the command
-    exit exit_status
-  end
-end
 
+    def create_file(path)
+        require 'fileutils'
+        dir = File.dirname(path)
+        
+        if !File.exists?(dir)
+            Dir.mkdir(File.join(Dir.pwd, path), 0700) #=> 0
+            puts "ceated file #{path}\n"
+        else
+            puts "existing file #{path}\n"
+        end
+    end
+    
+    def download(from,to)
+        ['net/http','uri'].each_with_index {|i| require i}
+        
+        counter = 0
+
+        url_base = from.split('/')[2]
+        url_path = '/'+from.split('/')[3..-1].join('/')
+             
+        puts "starting download for #{url_path}\n"
+        Net::HTTP.start(url_base) do |http|
+          begin
+            file = open(to, 'wb')
+            file.binmode
+
+            http.request_get(url_path) do |response|
+                size = 0
+                total = response.header["Content-Length"].to_f
+              response.read_body do |chunk|
+                    file << chunk
+                    counter += 1
+                    size += chunk.size
+                    new_progress = (size.to_f / total.to_f * 100.0).round(2)
+                    progress = "#" * (new_progress / 10)
+                    print "#{progress} --#{new_progress}%--\r"
+              end
+            end
+          ensure
+            file.close
+          end
+        end
+        puts "completed download for #{url_path}\n"
+    end
+
+end
